fe0cbbe0bc24f9462e9351f0f5d937f2e396db71
Torrex <flendik666@gmail.com>
2024-04-11 00:13:39 +0300

    added city search layout

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,53 @@
-<script setup>
+<script>
+  export default {
   
+  }
 </script>
 
 <template>
   <div class="container">
-
+    <div class="searchCity">
+      <label for="inputCity">Найти город</label>
+      <br>
+      <input type="text" id="inputCity" placeholder="Введите название города"> <button>Найти</button>
+    </div>
+    
   </div>
+
 </template>
 
 <style scoped>
+  .container {
+    border: 2px solid red;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    width: 30em;
+    height: 85vh;
+  }
+
+  .searchCity > label {
+    font-size: 35px;
+  }
+
+  .searchCity > input {
+    width: 20em;
+    height: 2.2em;
+    border: unset;
+  }
+
+  .searchCity > button {
+    width: 7em;
+    height: 2.2em;
+    border: unset;
+    cursor: pointer;
+    margin-left: .5em;
+  }
+
+  .searchCity > button:hover {
+    background-color: #ede6e6;
+  }
 
 </style>
